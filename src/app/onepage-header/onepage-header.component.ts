053d3169895e76f5d8e53558eac2a69227267037
umutbarandemir <baran292907@hotmail.com>
2024-01-11 13:14:59 +0300

    comp

--- src/app/onepage-header/onepage-header.component.ts
+++ src/app/onepage-header/onepage-header.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // html 
   selector: 'app-onepage-header',
+
+  // component: bağımsız çalışabilsin mi
   standalone: true,
+
+  // modül ekleme
   imports: [],
+
+  // Html5 Url
   templateUrl: './onepage-header.component.html',
+
+  // Css3 Url
   styleUrl: './onepage-header.component.css'
 })
 export class OnepageHeaderComponent {
