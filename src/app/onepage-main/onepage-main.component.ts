06ea97174423f4e8cbebdb8b49bd69850f660579
umutbarandemir <baran292907@hotmail.com>
2024-01-15 14:00:31 +0300

    card, main

--- src/app/onepage-main/onepage-main.component.ts
+++ src/app/onepage-main/onepage-main.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-onepage-main',
   standalone: true,
-  imports: [],
+  //*ngFor fonksiyonunun çalışması için bu CommonModule importlanmalı
+  imports: [CommonModule],
   templateUrl: './onepage-main.component.html',
   styleUrl: './onepage-main.component.css'
 })
 export class OnepageMainComponent {
+  /* 
+                      <div class="col-sm-12 col-md-3 col-lg-4 col-3">
+                        <div class="card">
+                            <img class="card-img-top" src="https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_1280.jpg" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Title</h4>
+                                <p class="card-text">Text</p>
+                            </div>
+                        </div>
+                    </div>
+  */
 
+  blogcard:any[]=[{
+    id:1, title:"Title-1",text:"Text-1", description:"Açıklama-1", picture:"https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_1280.jpg"
+  },
+  {
+    id:2, title:"Title-2",text:"Text-2", description:"Açıklama-2", picture:"https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_1280.jpg"
+  },
+  {
+    id:3, title:"Title-3",text:"Text-3", description:"Açıklama-3", picture:"https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_1280.jpg"
+  },
+  {
+    id:4, title:"Title-4",text:"Text-4", description:"Açıklama-4", picture:"https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_1280.jpg"
+  },
+  {
+    id:5, title:"Title-5",text:"Text-5", description:"Açıklama-5", picture:"https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_1280.jpg"
+  },
+  {
+    id:6, title:"Title-6",text:"Text-6", description:"Açıklama-6", picture:"https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_1280.jpg"
+  }];
 }
