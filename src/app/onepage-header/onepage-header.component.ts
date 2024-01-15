ec3475fb6d04bf7911e26a3edd97b26e5a329094
umutbarandemir <baran292907@hotmail.com>
2024-01-15 12:20:00 +0300

    header

--- src/app/onepage-header/onepage-header.component.ts
+++ src/app/onepage-header/onepage-header.component.ts
@@ -1,46 +1,67 @@
+// import
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  // html 
+  // html
   selector: 'app-onepage-header',
 
   // component: bağımsız çalışabilsin mi
   standalone: true,
 
   // modül ekleme
-  imports: [],
+  imports: [CommonModule],
 
   // Html5 Url
   templateUrl: './onepage-header.component.html',
 
   // Css3 Url
-  styleUrl: './onepage-header.component.css'
+  styleUrl: './onepage-header.component.css',
 })
 export class OnepageHeaderComponent {
-  homepage:String = "MainPage29";
-  
+  homePage: String = 'Anasayfa 44';
 
-  navBar:{
-    isLoggedin:boolean;
-    logo:String;
-    link:String;
-    search:{};
-    navbarLink:any[];
-  } = { 
-    isLoggedin:true,
-    logo:"",
-    link:"",
-    search:{
-      name:"Search",
-      placeholder:"Search",
-      type:"text",
+  // navBar adında bir obje oluşturunuz.
+  // Logo(string), link(string), search, attribute oluşturuyorsunuz.
+  // Link (dizi)
+  // navBar:{}={};
+  navBar: {
+    isLogin: boolean;
+    // logo: string, (fontAwesome,picture,link,title)
+    logo: {
+      logoName: string;
+      logoFontAwesome: string;
+      logoPicture: string;
+      logoLink: string;
+      logoTitle: string;
+    };
+    navbar: Array<any>;
+    search: {};
+  } = {
+    isLogin: true,
+    logo: {
+      logoName: 'Logo',
+      logoFontAwesome: 'fa-solid fa-code-branch',
+      logoPicture: 'https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_1280.jpg',
+      logoLink: '/',
+      logoTitle: 'Title',
     },
-    navbarLink:[
-      {id:1,name:"MainPage",link:"/",fontAwsome:"font-1"},
-      {id:2,name:"AboutMe",link:"/abouttme",fontAwsome:"font-2"},
-      {id:3,name:"Blog",link:"/blog",fontAwsome:"font-3"},
-      {id:4,name:"Contact",link:"/contact",fontAwsome:"font-4"}],
-  }
-  
-
-}
+    //Anasayfa,AboutMe,Blog,Contact  name: Anasayfa, link: http://, fontAwesome
+    navbar: [
+      {
+        id: 1,
+        name: 'Anasayfa',
+        link: '/',
+        fontAwesome: 'fa-solid fa-house-chimney',
+      },
+      { id: 2, name: 'AboutMe', link: '/about-me', fontAwesome: 'font-2' },
+      { id: 3, name: 'Blog', link: '/blog', fontAwesome: 'font-3' },
+      { id: 4, name: 'Contact', link: '/contact', fontAwesome: 'font-4' },
+    ],
+    search: {
+      name: 'Search',
+      placeholder: 'Arama için ...',
+      type: 'text',
+    },
+  }; //navBar Object
+} //end OnepageHeaderComponent
